127d4e52b5b5cd51e26e9238a94419e19f32b8fe
YSQ-888 <shiqi7046@163.com>
2024-12-23 14:46:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-from transform import preprocess_data
-X_train, Y_train = preprocess_data('../may14_feats4.csv')
 
-from train_model import train_model
-from keras.utils import to_categorical
-
-Y_train = to_categorical(Y_train, num_classes=3)
-model, history, x_test, y_test = train_model(X_train, Y_train)
-
-from test_model import evaluate_model, plot_training
-
-evaluate_model(model, x_test, y_test)
-plot_training(history)
